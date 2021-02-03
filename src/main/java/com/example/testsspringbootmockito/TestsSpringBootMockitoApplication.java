56bd343d356ae0cf84e2fa2c47ba3996828fba2c
mucheniski <mucheniski@>
2021-02-03 17:11:08 -0300

    first commit

---
+++ src/main/java/com/example/testsspringbootmockito/TestsSpringBootMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.testsspringbootmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsSpringBootMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsSpringBootMockitoApplication.class, args);
+	}
+
+}
